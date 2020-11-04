ec6d826b41fcca876146867660c77a4bb080e740
ZXB <zxb9604@163.com>
2020-11-04 13:42:56 +0800

    ignore

---
+++ bbx-blog/src/test/java/com/bbq/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbq.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- model/src/main/java/com/modeltest/model/Controller/ModelControlller.java
+++ model/src/main/java/com/modeltest/model/Controller/ModelControlller.java
@@ -20,7 +20,7 @@ public class ModelControlller {
 
     @GetMapping("/")
     public String index() {
-        return "model-htm";
+        return "login";
     }
 
     @PostMapping("/connect")
