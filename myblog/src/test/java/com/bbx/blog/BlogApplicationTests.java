5875491326113bf4ddee55ec998664b200c06d65
ZXB <zxb9604@163.com>
2020-11-10 17:49:05 +0800

    ignore

---
+++ myblog/src/test/java/com/bbx/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbx.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
